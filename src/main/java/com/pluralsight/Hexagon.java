fc8ae1e71723a3b5678c9926369d71057c959890
ErinnS <esummers@appdev.yearup.org>
2023-11-20 14:59:32 -0500

    TurtlePainter

--- src/main/java/com/pluralsight/Hexagon.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight;
-
-public class Hexagon extends Shape{
-    public Hexagon(String turtle, double x, double y, String color, double width) {
-        super(turtle, x, y, color, width);
-    }
-}
